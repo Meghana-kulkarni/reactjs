4e12e8ef810cf1edcdeaf68abd0230a26caf7e47
meghana <meghanakulkarni2002@gmail.com>
2025-12-26 02:49:19 +0530

    3rd commit

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function App() {
+  
+const [color, setColor] = useState("olive")
+  return (
+    <>
+      <div className="w-full h-screen duration-200" style = {{backgroundColor:color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+            <button className="ouline-none px-4 rounded-full text-white" style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
+                        <button className="ouline-none px-4 rounded-full text-white" style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>Blue</button>
+                        <button className="ouline-none px-4 rounded-full text-white" style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>Green</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
